2f096c5a87d754e3bc3a1053763b051175a6ba56
zhangzhicheng@cysignet.com <zachxy@2017>
2019-03-22 12:15:30 +0800

    修改微信签名

--- src/com/common/wx/WeiXinSignUtil.java
+++ src/com/common/wx/WeiXinSignUtil.java
@@ -4,6 +4,8 @@ import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.UUID;
 
+import org.apache.poi.util.SystemOutLogger;
+
 public class WeiXinSignUtil {
 
 	private static String token = "1314wxafd1d8527f4f2020";
@@ -34,6 +36,19 @@ public class WeiXinSignUtil {
 		return checktext != null ? checktext.equals(signature.toUpperCase()) : false;
 	}
 
+//	public static void main(String[] args) {
+//		// signature=8e07f66e22aeb2995cd2fe733345df15c0bb7ce2&echostr=2420431578604568574&timestamp=1553227518&nonce=1432642647
+//		String signature = "8e07f66e22aeb2995cd2fe733345df15c0bb7ce2";
+//		String echostr = "2420431578604568574";
+//		String timestamp ="1553227518";
+//		String nonce="1432642647";
+//		if(checkSignature(signature,timestamp,nonce)) {
+//			System.out.println("签名通过");
+//		}else {
+//			System.out.println("签名错误");
+//		}
+//	}
+
 	/**
 	 * 生成签名的随机串
 	 * 
@@ -58,22 +73,41 @@ public class WeiXinSignUtil {
 	 * @return
 	 */
 	public static String sha1Hex(String str) {
-		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-		try {
-			MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
-			mdTemp.update(str.getBytes("UTF-8"));
-			byte[] md = mdTemp.digest();
-			int j = md.length;
-			char buf[] = new char[j * 2];
-			int k = 0;
-			for (int i = 0; i < j; i++) {
-				byte byte0 = md[i];
-				buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
-				buf[k++] = hexDigits[byte0 & 0xf];
-			}
-			return new String(buf);
-		} catch (Exception e) {
-			return null;
-		}
+		 try {
+             MessageDigest md = MessageDigest.getInstance("SHA-1");
+             //对接后的字符串进行sha1加密
+             byte[] digest = md.digest(str.getBytes());
+             return byteToStr(digest);
+         } catch (Exception e){
+             e.printStackTrace();
+         }
+		 return "";
 	}
+	
+	/**
+     * 将字节数组转化我16进制字符串
+     * @param byteArrays 字符数组
+     * @return 字符串
+     */
+    private static String byteToStr(byte[] byteArrays){
+        String str = "";
+        for (int i = 0; i < byteArrays.length; i++) {
+            str += byteToHexStr(byteArrays[i]);
+        }
+        return str;
+    }
+
+    /**
+     *  将字节转化为十六进制字符串
+     * @param myByte 字节
+     * @return 字符串
+     */
+    private static String byteToHexStr(byte myByte) {
+        char[] Digit = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] tampArr = new char[2];
+        tampArr[0] = Digit[(myByte >>> 4) & 0X0F];
+        tampArr[1] = Digit[myByte & 0X0F];
+        String str = new String(tampArr);
+        return str;
+    }
 }

83336d11d137a4c9fc867e81e3a5799d6052048d
zhangzhicheng <zhangzhicheng@hehe>
2019-03-25 00:41:29 +0800

    修改请求方式

--- src/com/zach/gasTrade/controller/OrderFinanceStatisticsController.java
+++ src/com/zach/gasTrade/controller/OrderFinanceStatisticsController.java
@@ -94,12 +94,14 @@ public class OrderFinanceStatisticsController extends CommonController {
 		return result;
 	}
 
-	@RequestMapping(value = "/orderFinanceStatistics/exportOrderStatisticsList", method = RequestMethod.GET)
+	@RequestMapping(value = "/orderFinanceStatistics/exportOrderStatisticsList", method = RequestMethod.POST)
 	public void exportOrderStatistics(HttpServletRequest request, HttpServletResponse response,
-			OrderFinanceStatisticsVo filterMask) {
+			@RequestBody Map<String, String> param, OrderFinanceStatisticsVo filterMask) {
 		PageResult result = PageResult.initResult();
 
-		String searchStartDate = request.getParameter("searchStartDate");
+		// String searchStartDate = request.getParameter("searchStartDate");
+		// String searchEndDate = request.getParameter("searchEndDate");
+		String searchStartDate = param.get("searchStartDate");
 		String searchEndDate = request.getParameter("searchEndDate");
 		if (StringUtil.isNotNullAndNotEmpty(searchStartDate)) {
 			// filterMask.setDate(searchDate.substring(0, 10));

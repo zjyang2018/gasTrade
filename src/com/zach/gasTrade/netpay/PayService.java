b65631e26154ee93b24eae326b3479f7d74061eb
Administrator <Administrator@iZhbe0v9zvciebZ>
2019-04-14 20:46:47 +0800

    去掉pay bean spring创建

--- src/com/zach/gasTrade/netpay/PayService.java
+++ src/com/zach/gasTrade/netpay/PayService.java
@@ -19,13 +19,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zach.gasTrade.vo.ProductVo;
 
-@Component
+//@Component
 public class PayService {
 	private Logger logger = Logger.getLogger(getClass());
 
